9bbf1dd57224cb04bb744f4954d10d636ef0f4b5
Mimmo b <mimmo.baluyut@icloud.com>
2021-08-03 07:13:44 -0400

    Initial commit

---
+++ src/me/pineman/MaxConsecutiveOnes.java
@@ -0,0 +1,26 @@
+package me.pineman;
+
+public class MaxConsecutiveOnes {
+
+    public static void main(String[] args) {
+        int nums[] = {1,1,0,1,1,1};
+	    int answer = findMaxConsecutiveOnes(nums);
+        System.out.println(answer);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int ones = 0;
+        int maxOnes = 0;
+        for(int num : nums) {
+            if(num == 1) {
+                ones++;
+            } else {
+                ones=0;
+            }
+            if(ones>maxOnes){
+                maxOnes=ones;
+            }
+        }
+        return maxOnes;
+    }
+}
